7017bacbe046c92d098ff610b1ee61752f7495e2
Carl Egbert and Caleb Stokka <caleb@>
2016-06-09 16:11:10 -0700

    handle corner cases and bugs

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,31 @@
 var decToOtherCase = function(dec, caseInput){
-  // debugger;
-  var charKey = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"]
-  var caseArray = [];
-  var resultArray=[0,0,0,0,0,0,0,0,0,0];
-  for (var i = 9 ; i >= 0 ; i--) {
-    caseArray.push(Math.pow(caseInput,i));
-  }
-  console.log(caseArray);
-  for (var index = 0 ; index < 10 ; index++) {
-    while (dec >= caseArray[index]) {
-      // debugger;
-      dec -= caseArray[index];
-      resultArray[index]++;
+  if (dec < 1) {
+    return "Please enter a positive integer";
+  } else if (caseInput > 16 || caseInput < 2) {
+    return "Case must be between 2 and 16"
+  } else if (dec > (Math.pow(caseInput,10))){
+    return "That number is too big."
+  } else {
+    // debugger;
+    var charKey = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"]
+    var caseArray = [];
+    var resultArray=[0,0,0,0,0,0,0,0,0,0];
+    for (var i = 9 ; i >= 0 ; i--) {
+      caseArray.push(Math.pow(caseInput,i));
     }
+    for (var index = 0 ; index < 10 ; index++) {
+      while (dec >= caseArray[index]) {
+        // debugger;
+        dec -= caseArray[index];
+        resultArray[index]++;
+      }
+    }
+    var resultString = ""
+    for (var i = 0; i < resultArray.length; i++) {
+      resultString+=charKey[resultArray[i]];
+    }
+    return resultString;
   }
-  var resultString = ""
-  for (var i = 0; i < resultArray.length; i++) {
-    resultString+=charKey[resultArray[i]];
-  }
-  console.log(resultArray);
-  console.log(dec);
-  console.log(resultString);
 }
 
 
@@ -32,7 +37,5 @@ $(document).ready(function(){
     var caseInput = parseInt($("#caseInput").val());
     $("#result").append("<p>"+decToOtherCase(value,caseInput)+"</p>");
   });
-
-  decToOtherCase(987987908273,16);
 
 });
